cc114e5acb7451784d0e02b2da966a9d87bb0389
Lopes51789 <gablopesc10@gmail.com>
2025-01-09 12:02:59 -0300

    Exercicio3 completado
    sem testes unitarios

---
+++ exercicio3.py
@@ -0,0 +1,45 @@
+import json
+
+MAX_INT_VALUE = 999999999999
+MIN_INT_VALUE = -1
+
+if __name__ == '__main__':
+    f = open('dados.json', 'r')
+    dados = json.load(f)#leio os dados do json
+
+    faturas = {}
+    for dado in dados:
+        faturas[dado['dia']] = dado['valor']
+
+    f.close()
+
+    max_dia = -1
+    max_valor = MIN_INT_VALUE
+    min_dia = -1
+    min_valor = MAX_INT_VALUE
+    
+    mean_acm = 0
+    mean_dias = 0
+
+    for dia in faturas:
+        mean_acm += faturas[dia]
+        if faturas[dia] > max_valor:
+            max_dia = dia
+            max_valor = faturas[dia]
+        if faturas[dia] < min_valor:
+            min_dia = dia
+            min_valor = faturas[dia]
+        #print(f"{dia}: {faturas[dia]}")
+
+    mean_acm = mean_acm / len(faturas)
+
+    for dia in faturas:
+        if faturas[dia] > mean_acm:
+            mean_dias += 1
+
+
+    
+    print(f"O maior valor foi de R${max_valor:.2f} no dia {max_dia}"
+          f"\nO menor valor foi de R${min_valor:.2f} no dia {min_dia}"
+          f"\nO valor medio foi de R${mean_acm:.2f}"
+          f"\nO numero de dias acima do valor medio foi de {mean_dias}")

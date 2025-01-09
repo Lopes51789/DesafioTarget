66851bab5663fdc279dd2bef885fa4de47534a43
Lopes51789 <gablopesc10@gmail.com>
2025-01-09 12:13:31 -0300

    Fix Exercicio3, adicionei comentarios em Exercicio2, Exercicio3 e Exercicio5

--- exercicio3.py
+++ exercicio3.py
@@ -1,7 +1,17 @@
 import json
-
 MAX_INT_VALUE = 999999999999
 MIN_INT_VALUE = -1
+
+"""
+Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+
+IMPORTANTE:
+a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
+b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
+"""
 
 if __name__ == '__main__':
     f = open('dados.json', 'r')
@@ -22,7 +32,10 @@ if __name__ == '__main__':
     mean_dias = 0
 
     for dia in faturas:
-        mean_acm += faturas[dia]
+        if faturas[dia] != 0:
+            mean_acm += faturas[dia]
+            mean_dias += 1
+
         if faturas[dia] > max_valor:
             max_dia = dia
             max_valor = faturas[dia]
@@ -31,15 +44,16 @@ if __name__ == '__main__':
             min_valor = faturas[dia]
         #print(f"{dia}: {faturas[dia]}")
 
-    mean_acm = mean_acm / len(faturas)
-
+    mean_acm = mean_acm / mean_dias
+    dias_validos = 0
+    
     for dia in faturas:
         if faturas[dia] > mean_acm:
-            mean_dias += 1
+            dias_validos += 1
 
 
     
     print(f"O maior valor foi de R${max_valor:.2f} no dia {max_dia}"
           f"\nO menor valor foi de R${min_valor:.2f} no dia {min_dia}"
           f"\nO valor medio foi de R${mean_acm:.2f}"
-          f"\nO numero de dias acima do valor medio foi de {mean_dias}")
+          f"\nO numero de dias acima do valor medio foi de {dias_validos}")
